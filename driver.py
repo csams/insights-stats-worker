b1bb81ab4409b5ba9df891a8cbc02c0cf6761de0
Christopher Sams <csams@redhat.com>
2019-12-10 13:15:04 -0600

    Change to use rabbitmq publisher instead of CLI.
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- driver.py
+++ driver.py
@@ -5,18 +5,19 @@ import yaml
 
 Loader = getattr(yaml, "CSafeLoader", yaml.SafeLoader)
 
-params = pika.ConnectionParameters(host='localhost', port=5672)
+params = pika.ConnectionParameters(host="localhost", port=5672)
 connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
-channel.queue_declare(queue='test_job')
+channel.queue_declare(queue="test_job")
 
 root = "/home/csams/Downloads/archives/insights/"
-archives = []
+archives = [
+]
 
 for archive in archives:
     path = os.path.join(root, archive)
     msg = {"url": path}
     body = yaml.dump(msg)
-    channel.basic_publish(exchange='', routing_key="test_job", body=body)
+    channel.basic_publish(exchange="", routing_key="test_job", body=body)
 connection.close()
